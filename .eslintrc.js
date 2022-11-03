2a8c2b9c5ff41846858438bd0344ac4d5a91c57c
hontaku <hntk@nasbeam.com>
2022-11-04 00:28:44 +0900

    nuxtの初期構築

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  rules: {
+    'no-irregular-whitespace': ['error', { skipRegExps: true, skipComments: true, skipStrings: true }],
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-implicit-any-catch': 'off',
+    'vue/multi-word-component-names': 0
+  },
+  overrides: [
+    {
+      files: ['cdk/**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './cdk/tsconfig.json'
+      }
+    }
+  ]
+}
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: false,
+  arrowParens: 'always',
+  singleQuote: true,
+  printWidth: 120,
+  trailingComma: 'none'
+}
