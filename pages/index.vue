7428418a72421e4fe8f7b66741ceae642e73889a
hontaku <hntk@nasbeam.com>
2022-11-04 00:29:21 +0900

    お試しページとAPIを作成

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  const date = ref("")
+  onMounted(async()=>{
+    const response = await $fetch('/api/date');
+    date.value = response.date
+  })
+
+</script>
+
+<template>
+  <div>
+    Hello world
+  </div>
+  <div>{{date}}</div>
+</template>
